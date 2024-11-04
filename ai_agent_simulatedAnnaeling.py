80474ed3bb4b36f4dca2758351b64d236e665045
dzikrimrputra <18221144@std.stei.itb.ac.id>
2024-11-04 09:37:51 +0700

    add selected move simulated annaeling

--- ai_agent_simulatedAnnaeling.py
+++ ai_agent_simulatedAnnaeling.py
@@ -4,71 +4,84 @@ import math
 from othello_game import OthelloGame
 
 def scheduling_function(end_time, max_time):
-    return (end_time - time.time()) / max_time
+    return (end_time - time.time())
 
-def simulated_annealing(game, max_time=4):
+def simulated_annealing(game, max_time=4, move_probability=0.5):
     start_time = time.time()
     end_time = start_time + max_time
+    selected_move = []
 
     current_move = random.choice(game.get_valid_moves())
-    current_score = evaluate_game_state(game, current_move)
+    first_game_eval = OthelloGame(player_mode=game.player_mode)
+    first_game_eval.board = [row[:] for row in game.board]
+    first_game_eval.current_player = game.current_player
+    first_game_eval.make_move(*current_move)
+    current_state_value = evaluate_game_state(first_game_eval)
 
-
-    T = 1.0 
+    T = 4.0 
     while T > 0:
         T = scheduling_function(end_time = end_time, max_time = max_time) 
         if(T<=0):
             break
         new_move = random.choice(game.get_valid_moves())
-        new_score = evaluate_game_state(game, new_move)
+        new_game = OthelloGame(player_mode=game.player_mode)
+        new_game.board = [row[:] for row in game.board]
+        new_game.current_player = game.current_player
+        new_game.make_move(*new_move)
+        new_state_value = evaluate_game_state(new_game)
 
-        if new_score > current_score:
-            current_move = new_move
-            current_score = new_score
-
-        else:
-            delta_score = new_score - current_score
-            acceptance_probability = math.exp(delta_score / T)
-            if acceptance_probability > 0.5:
+        if new_move not in selected_move:
+            if new_state_value > current_state_value:
                 current_move = new_move
-                current_score = new_score
+                current_state_value = new_state_value
+
+            else:
+                probability = math.exp((new_state_value - current_state_value) / T)
+                print((new_state_value - current_state_value) / T)
+                print(probability)
+                if probability > move_probability:
+                    current_move = new_move
+                    current_state_value = new_state_value
+            selected_move.append(new_move)
 
 
     return current_move
 
 
-def evaluate_game_state(game, move):
-    new_game = OthelloGame(player_mode=game.player_mode)
-    new_game.board = [row[:] for row in game.board]
-    new_game.current_player = game.current_player
-    new_game.make_move(*move)
-    
+def evaluate_game_state(game):
+    # Evaluation weights for different factors
     coin_parity_weight = 1.0
     mobility_weight = 2.0
     corner_occupancy_weight = 5.0
     stability_weight = 3.0
     edge_occupancy_weight = 2.5
 
-    player_disk_count = sum(row.count(new_game.current_player) for row in new_game.board)
-    opponent_disk_count = sum(row.count(-new_game.current_player) for row in new_game.board)
+    # Coin parity (difference in disk count)
+    player_disk_count = sum(row.count(game.current_player) for row in game.board)
+    opponent_disk_count = sum(row.count(-game.current_player) for row in game.board)
     coin_parity = player_disk_count - opponent_disk_count
 
-    player_valid_moves = len(new_game.get_valid_moves())
+    # Mobility (number of valid moves for the current player)
+    player_valid_moves = len(game.get_valid_moves())
     opponent_valid_moves = len(
-        OthelloGame(player_mode=-new_game.current_player).get_valid_moves()
+        OthelloGame(player_mode=-game.current_player).get_valid_moves()
     )
     mobility = player_valid_moves - opponent_valid_moves
 
+    # Corner occupancy (number of player disks in the corners)
     corner_occupancy = sum(
-        new_game.board[i][j] for i, j in [(0, 0), (0, 7), (7, 0), (7, 7)]
+        game.board[i][j] for i, j in [(0, 0), (0, 7), (7, 0), (7, 7)]
     )
 
-    stability = calculate_stability(new_game)
+    # Stability (number of stable disks)
+    stability = calculate_stability(game)
 
-    edge_occupancy = sum(new_game.board[i][j] for i in [0, 7] for j in range(1, 7)) + sum(
-        new_game.board[i][j] for i in range(1, 7) for j in [0, 7]
+    # Edge occupancy (number of player disks on the edges)
+    edge_occupancy = sum(game.board[i][j] for i in [0, 7] for j in range(1, 7)) + sum(
+        game.board[i][j] for i in range(1, 7) for j in [0, 7]
     )
 
+    # Combine the factors with the corresponding weights to get the final evaluation value
     evaluation = (
         coin_parity * coin_parity_weight
         + mobility * mobility_weight
@@ -79,8 +92,8 @@ def evaluate_game_state(game, move):
 
     return evaluation
 
-
 def calculate_stability(game):
+
     def neighbors(row, col):
         return [
             (row + dr, col + dc)
